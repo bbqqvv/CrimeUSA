00c12dd782dbed4bbe12c7a91ff3da41381c3547
phamkimkhuong <phamkhuong436@gmail.com>
2025-07-03 11:03:41 +0700

    feat: initialize multi-module project structure with common, investigation, and suspect services

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+
+### 1. Cấu trúc dự án
+
+- multi-module-project: chứa các module con của dự án (cấu hình version, plugin, dependency)
+- common-service: chứa các class dùng chung cho các module khác
+
+### 2. Chạy dự án
+- build common-service thành jar để các module khác có thể sử dụng
---
+++ backend/suspect-service/src/main/java/com/backend/suspectservice/SuspectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.suspectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuspectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuspectServiceApplication.class, args);
+	}
+
+}
