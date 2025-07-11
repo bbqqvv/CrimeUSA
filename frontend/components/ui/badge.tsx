f433e058c341df11669dd8ab3c26e597725d5960
uQHan <hanng.22ite@vku.udn.vn>
2025-07-11 18:09:53 +0700

    weird shadcn component file that doesnt want to be staged, I have no idea what the problem is but it seem only git add . can stage it.

--- frontend/components/ui/badge.tsx
+++ frontend/components/ui/badge.tsx
@@ -1,6 +1,6 @@
 // src/components/ui/Badge.tsx
 interface BadgeProps {
-  status: 'Approved' | 'Pending' | 'Rejected' | 'Not Urgent' | 'Urgent';
+  status: 'Approved' | 'Pending' | 'Rejected';
 }
 
 const Badge = ({ status }: BadgeProps) => {
@@ -9,8 +9,6 @@ const Badge = ({ status }: BadgeProps) => {
     Approved: 'bg-green-100 text-green-800',
     Pending: 'bg-yellow-100 text-yellow-800',
     Rejected: 'bg-red-100 text-red-800',
-    'Not Urgent': 'bg-gray-100 text-gray-800',
-    Urgent: 'bg-orange-100 text-orange-800',
   };
 
   return <span className={`${baseClasses} ${statusClasses[status]}`}>{status}</span>;
